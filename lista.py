bcb9bb22496f0699f6b7fcc1bbffc5d2db576f65
Larissa Farias <101722899+Laramina984@users.noreply.github.com>
2023-04-17 14:44:14 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,22 @@
+#para lista usamos []
+#index:     0      1   2  3
+lista = ["carro",True ,2,3.5]
+print(type(lista))
+print(lista)
+#a tupla usa ()
+#index:    0       1    2   3
+tupla = ("carro", True, 2, 3.5)
+print(tupla)
+print(tupla)
+
+#adiconario usa {}, chave: seguida de valor
+#index:          0                   1              2           3
+adicionario = {"strig": "valor", "boolean":True, "int": 2, "float": 3.6}
+print(type(adicionario))
+print(adicionario)
+
+#conjunto usa {} só
+#index:        0      1    2   3
+conjunto = {"carro", True, 2, 3.4}
+print(type(conjunto))
+print(conjunto)
